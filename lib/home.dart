02c577e445494b808e761813ab236f2a49abff2c
Pooh555 <peerasit.lu@gmail.com>
2024-06-22 16:08:34 +0700

    Make theme adjustable

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+String theme = "dark";
+
 // Initiaize Theme class (dark theme is the default theme)
 class Theme {
-  final color_1 = const Color.fromARGB(255, 43, 43, 43);
-  final color_2 = const Color.fromARGB(255, 138, 138, 138);
-  final color_3 = const Color.fromRGBO(253, 253, 253, 1);
+  String theme = "dark"; // Set default theme as dark
+
+  dynamic color_1; // Primary/Background color
+  dynamic color_2; // Secondary color
+  dynamic color_3; // Tertiary color
+  dynamic textColor_1; // Primary text color
+
+  // Theme object constructor
+  Theme(String theme) {
+    if (theme == "light") {
+      color_1 = const Color.fromARGB(255, 255, 255, 255);
+      color_2 = const Color.fromARGB(255, 197, 243, 255);
+      color_3 = const Color.fromARGB(255, 232, 250, 255);
+      textColor_1 = const Color.fromARGB(255, 0, 0, 0);
+    }
+    if (theme == "dark") {
+      color_1 = const Color.fromARGB(255, 43, 43, 43);
+      color_2 = const Color.fromARGB(255, 138, 138, 138);
+      color_3 = const Color.fromRGBO(253, 253, 253, 1);
+      textColor_1 = const Color.fromARGB(255, 255, 255, 255);
+    }
+  }
 }
 
 void main() {
@@ -15,7 +36,7 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   // Initialize theme
-  var currentTheme = Theme();
+  var currentTheme = Theme(theme);
 
   // This widget is the root of the application.
   @override
