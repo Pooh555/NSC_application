df22b30e0d2cd0545962aa0a3752e6037cf275fb
Pooh555 <peerasit.lu@gmail.com>
2024-06-23 21:22:46 +0700

    chatbot done

--- lib/chatbot.dart
+++ lib/chatbot.dart
@@ -1,18 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
-class ChatBotPage extends StatelessWidget {
+class ChatBotPage extends StatefulWidget {
   const ChatBotPage({super.key});
 
   @override
+  State<ChatBotPage> createState() => ChatBotPageState();
+}
+
+class ChatBotPageState extends State<ChatBotPage> {
+  List<Content> history = [];
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode();
+  bool _loading = false;
+  static const _apiKey =
+      'AIzaSyA0jLrpwuZ44RkIV_l7HT8Z4j_gdySVv14'; // https://ai.google.dev/ (Get API key from this link)
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.minScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+      model: 'gemini-pro',
+      apiKey: _apiKey,
+    );
+    _chat = _model.startChat();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatBot Page'),
+        title: const Text('Gemini AI'),
       ),
-      body: const Center(
-        child: Text(
-          'This is the ChatBot page',
-          style: TextStyle(fontSize: 24),
+      body: Stack(
+        children: [
+          ListView.separated(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 90),
+            itemCount: history.reversed.length,
+            controller: _scrollController,
+            reverse: true,
+            itemBuilder: (context, index) {
+              var content = history.reversed.toList()[index];
+              var text = content.parts
+                  .whereType<TextPart>()
+                  .map<String>((e) => e.text)
+                  .join('');
+              return MessageTile(
+                sendByMe: content.role == 'user',
+                message: text,
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                height: 15,
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(top: BorderSide(color: Colors.grey.shade200))),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 55,
+                      child: TextField(
+                        controller: _textController,
+                        autofocus: true,
+                        focusNode: _textFieldFocus,
+                        decoration: InputDecoration(
+                            hintText: 'Ask me anything...',
+                            hintStyle: const TextStyle(color: Colors.grey),
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 15),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        history.add(
+                            Content('user', [TextPart(_textController.text)]));
+                      });
+                      _sendChatMessage(_textController.text, history.length);
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration:
+                          BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                        BoxShadow(
+                            offset: const Offset(1, 1),
+                            blurRadius: 3,
+                            spreadRadius: 3,
+                            color: Colors.black.withOpacity(0.05))
+                      ]),
+                      child: _loading
+                          ? const Padding(
+                              padding: EdgeInsets.all(15.0),
+                              child: CircularProgressIndicator.adaptive(
+                                backgroundColor: Colors.white,
+                              ),
+                            )
+                          : const Icon(
+                              Icons.send_rounded,
+                              color: Colors.white,
+                            ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message, int historyIndex) async {
+    setState(() {
+      _loading = true;
+      _textController.clear();
+      _textFieldFocus.unfocus();
+      _scrollDown();
+    });
+
+    List<Part> parts = [];
+
+    try {
+      var response = _chat.sendMessageStream(
+        Content.text(message),
+      );
+      await for (var item in response) {
+        var text = item.text;
+        if (text == null) {
+          _showError('No response from API.');
+          return;
+        } else {
+          setState(() {
+            _loading = false;
+            parts.add(TextPart(text));
+            if ((history.length - 1) == historyIndex) {
+              history.removeAt(historyIndex);
+            }
+            history.insert(historyIndex, Content('model', parts));
+          });
+        }
+      }
+    } catch (e, t) {
+      print(e);
+      print(t);
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  void _showError(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: SelectableText(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class MessageTile extends StatelessWidget {
+  final bool sendByMe;
+  final String message;
+
+  const MessageTile({required this.sendByMe, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            color: sendByMe ? Colors.blueAccent : Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: sendByMe ? Colors.white : Colors.black,
+              fontSize: 16.0,
+            ),
+          ),
         ),
       ),
     );
