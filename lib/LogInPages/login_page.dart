35e3f9c03765b2c1968b1c3bca77ebd7db1e5773
Pooh555 <peerasit.lu@gmail.com>
2024-07-17 01:02:11 +0700

    Change icon in login pages

--- lib/LogInPages/login_page.dart
+++ lib/LogInPages/login_page.dart
@@ -69,15 +69,11 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 30),
-
                 // logo
-                const Icon(
-                  Icons.lock,
-                  size: 50,
-                ),
+                Image.asset("assets/images/app_icon.png",
+                    width: 150.0, height: 150.0),
 
-                const SizedBox(height: 50),
+                const SizedBox(height: 10),
 
                 // welcome back, you've been missed!
                 Text(
