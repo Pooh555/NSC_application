191cf80ed5165cd7e04f330c9d289c4358d4d482
Pooh555 <peerasit.lu@gmail.com>
2024-08-16 11:50:18 +0700

    Fix map issue

--- lib/hospital.dart
+++ lib/hospital.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class HospitalPage extends StatelessWidget {
+class HospitalPage extends StatefulWidget {
   const HospitalPage({super.key});
 
   @override
+  _HospitalPageState createState() => _HospitalPageState();
+}
+
+class _HospitalPageState extends State<HospitalPage> {
+  LatLng? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    // Request location permission
+    if (await Permission.location.request().isGranted) {
+      // Get the current position
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(13.7563, 100.5018),
-        initialZoom: 15.0,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-      ],
+    return Scaffold(
+      body: _currentPosition == null
+          ? const Center(child: CircularProgressIndicator())
+          : FlutterMap(
+              options: MapOptions(
+                initialCenter: _currentPosition!,
+                initialZoom: 15.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+            ),
     );
   }
 }
